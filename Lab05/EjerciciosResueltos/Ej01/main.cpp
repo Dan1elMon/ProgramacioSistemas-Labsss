4cd8a0f5f76677645224c7a9492f2bbb901ab961
Daniel Chura <dchuramo@unsa.edu.pe>
2024-06-11 11:10:44 -0500

    ejercicio 01 y 02 resueltos completados

---
+++ Lab05/EjerciciosResueltos/Ej01/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+struct persona{
+    char nombre[15];
+    char apellido[15];
+    int edad;
+    char sexo[15];
+    int telefono;
+};
+int main(){
+    persona amigo[5];
+    for (int i = 0; i < 5; i++){
+        cout <<"Escriba el Nombre "<<i+1<<":";
+        cin >> amigo[i].nombre;
+        cout <<"\nEscriba el Apellido "<<i+1<<":";
+        cin >> amigo[i].apellido;
+        cout <<"\nEscriba la Edad de "<<i+1<<":";
+        cin >> amigo[i].edad;
+        cout <<"\nEscriba el sexo "<<i+1<<":";
+        cin >> amigo[i].sexo;
+        cout <<"\nEscriba el Telefono de "<<i+1<<":";
+        cin >> amigo[i].telefono;
+        cout <<endl;
+    }
+    cout<<"El registro de personas que se introdujeron es: \n\n";
+    for (int i = 0; i < 5; i++){
+        cout<<"\t"<<amigo[i].nombre;
+        cout<<"\t"<<amigo[i].apellido;
+        cout<<"\t"<<amigo[i].edad;
+        cout<<"\t"<<amigo[i].sexo;
+        cout<<"\t"<<amigo[i].telefono<<"\n\n";
+    } 
+}
