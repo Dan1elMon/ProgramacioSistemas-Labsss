4155c6914a3b4b840be688530b974febe8d22bd4
Daniel Chura <dchuramo@unsa.edu.pe>
2024-06-17 17:56:28 -0500

    ej07 completo

--- Lab06/EjerciciosResueltos/Ej07/main.cpp
+++ Lab06/EjerciciosResueltos/Ej07/main.cpp
@@ -3,23 +3,31 @@
 #include <string>
 #include <cstdlib>
 using namespace std;
+
 int main()
 {
-string nombre;
-string linea;
-cout << "Dime el nombre del fichero: ";
-getline(cin, nombre);
-ifstream fichero(nombre.c_str());
-if( fichero.fail() )
-{
-cout << "No existe el fichero!" << endl;
-exit(1);
-}
-while (! fichero.eof())
-{
-getline(fichero,linea);
-if (! fichero.eof())
-cout << linea << endl;
-} fichero.close();
-return 0;
+    //declarando variables tipo string
+    string nombre;
+    string linea;
+    //pidiendo nmbre del fichero por teclado
+    cout << "Dime el nombre del fichero: ";
+    getline(cin, nombre);
+    //abiendo fichero
+    ifstream fichero(nombre.c_str());
+    //verificando si existe el fichero
+    if( fichero.fail() )
+    {
+        cout << "No existe el fichero!" << endl;
+        exit(1);
+    }
+    while (! fichero.eof())
+    {
+        //imprimiendo linea del ichero si existe
+        getline(fichero,linea);
+       // if (!fichero.eof())
+        cout << linea << endl;
+    }
+    //cerrando ficjero
+    fichero.close();
+    return 0;
 }
