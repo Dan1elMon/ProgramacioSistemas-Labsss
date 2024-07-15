c0f4e026361430e467cde26d0b16a006bb4107d3
Daniel Chura <dchuramo@unsa.edu.pe>
2024-07-15 17:40:49 -0500

    ejercicio 05

--- Lab09/EjerciciosResueltos/Ejercicio05/main.cpp
+++ Lab09/EjerciciosResueltos/Ejercicio05/main.cpp
@@ -2,21 +2,24 @@
 #include <iostream>
 #include <pthread.h>
 using namespace std;
+
 #define NUM_THREADS 1
-void* say_hello(void* args)
-{
-cout << "Hola mundo!" << endl;
+
+void* say_hello(void* args) {
+    cout << "Hola mundo!" << endl;
+    return NULL; // el thread uede retornar un valor
 }
-int main()
-{
-pthread_t tids[NUM_THREADS];
-for(int i = 0; i < NUM_THREADS; ++i)
-{
-int ret = pthread_create(&tids[i], NULL, say_hello, NULL);
-if (ret != 0)
-{
-cout << "pthread_create error: error_code=" << ret << endl;
+
+int main() {
+    pthread_t tids[NUM_THREADS]; // un arreglo para almacenar ids
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        int ret = pthread_create(&tids[i], NULL, say_hello, NULL);
+        if (ret != 0) {
+        cout << "Error en thread: codigo_error=" << ret << endl;
+        }
+    }
+
+    pthread_exit(NULL); // Main thread exits
 }
-}
-pthread_exit(NULL);
-}
+
